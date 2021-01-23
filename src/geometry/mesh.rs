b17c50d51a11a1558e656ca0148a6aa0d742c0fc
Alex Kuczala <kookzala@gmail.com>
2021-01-23 13:27:50 -0500

    added display methods, more normals progress

--- src/geometry/mesh.rs
+++ src/geometry/mesh.rs
@@ -1,20 +1,21 @@
 
-use super::{VectorTrait,VertIndex};
+use super::{VectorTrait,MatrixTrait,VertIndex};
+use std::fmt;
 
 type FacetIndex = VertIndex;
 
-trait FacetTrait : Clone {
+trait FacetTrait :  Clone + fmt::Display {
     fn map<F : Fn(FacetIndex) -> FacetIndex>(&self, f : F) -> Self;
 }
 
-#[derive(Copy,Clone,Debug)]
+#[derive(Clone,Debug)]
 struct Facet0<V: VectorTrait>{i: FacetIndex, normal: V}
 impl<V: VectorTrait> Facet0<V> {
     fn new(i : FacetIndex, normal: V) -> Self {
         Self{i, normal}
     }
 }
-#[derive(Copy,Clone,Debug)]
+#[derive(Clone,Debug)]
 struct Facet1<V: VectorTrait>{edge: (FacetIndex,FacetIndex), normal: V}
 impl<V: VectorTrait> Facet1<V> {
     fn new(vi0 : FacetIndex, vi1 : FacetIndex, normal: V) -> Self {
@@ -59,6 +60,51 @@ impl<V: VectorTrait> FacetTrait for Facet3<V> {
         Self{faceis: self.faceis.iter().map(|&x| f(x)).collect(), normal: self.normal}
     }
 }
+fn list_string<'a, T: 'a + fmt::Display + Copy>(iter: impl Iterator<Item = &'a T>) -> String {
+    let mut out = format!("");
+    for &v in iter {
+        out = format!("{}{} ",out,v);
+    }
+    out
+}
+fn list_stack_string<'a, T: 'a + fmt::Display>(iter: impl Iterator<Item = &'a T>) -> String {
+    let mut out = format!("");
+    for v in iter { 
+        out = format!("{}{}\n",out,v);
+    }
+    out
+}
+
+impl<V : VectorTrait> fmt::Display for Facet0<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        // let mut vec_str = format!("");
+        // for &n_i in self.normal.iter() {
+        //     vec_str = format!("{} ",n_i)
+        // }
+        let vec_str = list_string(self.normal.iter());
+        write!(f, "[{}], [{}]",self.i,vec_str)
+    }
+}
+impl<V : VectorTrait> fmt::Display for Facet1<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec_str = list_string(self.normal.iter());
+        write!(f, "[{},{}], [{}]",self.edge.0,self.edge.1,vec_str)
+    }
+}
+impl<V : VectorTrait> fmt::Display for Facet2<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec_str = list_string(self.normal.iter());
+        let i_str = list_string(self.edgeis.iter());
+        write!(f, "[{}], [{}]",i_str,vec_str)
+    }
+}
+impl<V : VectorTrait> fmt::Display for Facet3<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec_str = list_string(self.normal.iter());
+        let i_str = list_string(self.faceis.iter());
+        write!(f, "[{}], [{}]",i_str,vec_str)
+    }
+}
 
 #[derive(Clone,Debug)]
 struct FacetList<T : FacetTrait>(Vec<T>);
@@ -68,6 +114,9 @@ impl<T : FacetTrait> FacetList<T> {
     }
     fn map_index<F : Fn(FacetIndex) -> FacetIndex + Copy>(&self, f : F) -> Self {
         Self(self.0.iter().map(|x| x.map(f)).collect())
+    }
+    fn map<F : Fn(&T) -> T>(&self, f: F) -> Self {
+        Self(self.0.iter().map(|facet| f(facet)).collect())
     }
     fn concat(&self, other: &FacetList<T>) -> Self {
         let mut new = self.0.clone();
@@ -97,6 +146,24 @@ impl<T : FacetTrait> Index<FacetIndex> for FacetList<T> {
         &self.0[i]
     }
 }
+impl<T: FacetTrait> fmt::Display for FacetList<T> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let vec_str = list_stack_string(self.0.iter());
+        write!(f, "{}",vec_str)
+    }
+}
+
+fn make_perp_towards<V: VectorTrait>(perp_to: V, towards: V) -> V {
+    let perp_to_norm = perp_to.normalize();
+    (towards - perp_to_norm*towards.dot(perp_to_norm)).normalize()
+} 
+fn reflect_along<V : VectorTrait>(v: V, along: V) -> V {
+    v - along*2.0*v.dot(along.normalize())
+}
+// fn cross_along()
+// CrossAlong[v1_, v2_, along_] := Module[{cross = Cross[v1, v2]},
+//    If[cross.along > 0, cross, -cross]
+//    ];
 
 #[derive(Clone,Debug)]
 struct FacetComplex<V: VectorTrait>{
@@ -135,7 +202,13 @@ impl<V: VectorTrait> FacetComplex<V>{
         todo!()
     }
 }
+impl<V: VectorTrait> fmt::Display for FacetComplex<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "VERTS\n{}\nEDGES\n{}\nFACES\n{}\nVOLUMES\n{}\n",self.vertis, self.edges, self.faces, self.volumes)
+    }
+}
 //replicate mesh-test-p2.nb (in progress)
+//need to change the order of normal assignment
 
 pub fn extrude<V : VectorTrait>(mesh : &Mesh<V>, evec : V) -> Mesh<V> {
     let placeholder = V::zero();
@@ -143,15 +216,19 @@ pub fn extrude<V : VectorTrait>(mesh : &Mesh<V>, evec : V) -> Mesh<V> {
     let shifted_facets = mesh.facet_complex.shifted();
 
     let mut new_facets = FacetComplex::empty();
-    let vertis_far = new_facets.vertis.extend(&facets.vertis);
-    let vertis_close = new_facets.vertis.extend(&shifted_facets.vertis);
+    let vertis_far = new_facets.vertis.extend(
+        &facets.vertis.map(|facet| Facet0::new(facet.i, -evec.normalize()))
+    );
+    let vertis_close = new_facets.vertis.extend(
+        &shifted_facets.vertis.map(|facet| Facet0::new(facet.i, evec.normalize()))
+    );
 
     let far_edgeis = new_facets.edges.extend(&facets.edges.clone());
     let close_edgeis = new_facets.edges.extend(&shifted_facets.edges.clone());
     let long_edgeis = new_facets.edges.extend(&FacetList(
         vertis_far.iter()
             .zip(vertis_close.iter())
-            .map(|(&f,&sf)| Facet1::new(f,sf, placeholder))
+            .map(|(&far_i,&close_i)| Facet1::new(far_i,close_i, make_perp_towards(evec, new_facets.vertis[far_i].normal)))
             .collect()
     ));
 
@@ -215,32 +292,40 @@ impl<V: VectorTrait> Mesh<V> {
         }
     }
 }
+impl<V: VectorTrait> fmt::Display for Mesh<V> {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}",self.facet_complex)
+    }
+}
+
 
 #[test]
 fn test_extrude() {
-    use crate::vector::{Vec4};
+    use crate::vector::{Vec3,Vec4};
+    type VecN = Vec3;
     let point = Mesh{
-        verts: vec![Vec4::new(0.,0.,0.,0.)],
+        verts: vec![VecN::zero()],
         facet_complex : FacetComplex{
-            vertis: FacetList(vec![Facet0::new(0, Vec4::ones())]),
+            vertis: FacetList(vec![Facet0::new(0, VecN::ones().normalize())]),
             edges: FacetList(vec![]),
             faces: FacetList(vec![]),
             volumes: FacetList(vec![]),
         }
     };
-    let line = extrude(&point,Vec4::new(1.0,0.,0.,0.));
-    let square = extrude(&line,Vec4::new(0.0,1.,0.,0.));
-    let cube = extrude(&square,Vec4::new(0.,0.,1.0,0.));
-    let tess = extrude(&cube,Vec4::new(0.,0.,0.,1.0));
+    let id_mat = <VecN as VectorTrait>::M::id();
+    let line = extrude(&point,id_mat[0]);
+    let square = extrude(&line,id_mat[1]);
+    let cube = extrude(&square,id_mat[2]);
+    //let tess = extrude(&cube,Vec4::new(0.,0.,0.,1.0));
     println!("point");
-    println!("{:?}", point);
+    println!("{}", point);
     println!("line");
-    println!("{:?}", line);
+    println!("{}", line);
     println!("square");
-    println!("{:?}", square);
+    println!("{}", square);
     println!("cube");
-    println!("{:?}", cube);
-    println!("tess");
-    println!("{:?}", tess);
+    println!("{}", cube);
+    //println!("tess");
+    //println!("{}", tess);
     assert!(false)
 }
