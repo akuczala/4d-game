d7c313f223d0018d9d689bd49f12645372069b8d
Alex Kuczala <kookzala@gmail.com>
2023-07-16 23:45:31 -0400

    "simplify" test level builder

--- src/build_level/face_test.rs
+++ src/build_level/face_test.rs
@@ -1,6 +1,7 @@
 use crate::draw::FaceTexture;
 use crate::geometry::shape::buildshapes::{convex_shape_to_face_shape, ShapeBuilder};
 use crate::graphics::colors::*;
+use crate::vector::VecIndex;
 use crate::{components::StaticCollider, constants::PI};
 
 use specs::{Builder, World, WorldExt};
@@ -22,86 +23,68 @@ use crate::{
 
 use super::{insert_coin, insert_static_collider};
 
-fn build_test_walls<V>(build_shape: &ShapeEntityBuilderV<V>, world: &mut World)
-where
-    V: VectorTrait + Componentable,
-    V::SubV: Componentable,
-    V::M: Componentable,
-{
+fn build_wall<V: VectorTrait>(
+    pos: V,
+    rot: (VecIndex, VecIndex, Field),
+    color: Color,
+) -> impl Fn(ShapeEntityBuilderV<V>) -> ShapeEntityBuilderV<V> {
+    move |builder| {
+        builder
+            .with_translation(pos)
+            .with_rotation(rot.0, rot.1, rot.2)
+            .with_color(color)
+            .with_collider(Some(StaticCollider))
+    }
+}
+
+fn build_test_walls<'a, V: VectorTrait + 'a>(
+    build_shape: &'a ShapeEntityBuilderV<V>,
+) -> impl IntoIterator<Item = ShapeEntityBuilderV<V>> + 'a {
     let theta = PI / 6.0;
     let cos = theta.cos();
     let sin = theta.sin();
-    build_shape
-        .clone()
-        .with_translation(V::one_hot(-1) * (-1.0 - cos) + V::one_hot(1) * (sin - 1.0))
-        .with_rotation(-1, 1, PI / 2.0 - theta)
-        .with_color(RED)
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    build_shape
-        .clone()
-        .with_translation(V::one_hot(-1) * 1.0)
-        .with_rotation(0, -1, PI)
-        .with_color(GREEN)
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    build_shape
-        .clone()
-        .with_translation(V::one_hot(0) * 1.0)
-        .with_rotation(0, -1, PI / 2.)
-        .with_color(ORANGE)
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    build_shape
-        .clone()
-        .with_translation(-V::one_hot(0) * 1.0)
-        .with_rotation(0, -1, 3.0 * PI / 2.)
-        .with_color(CYAN)
-        .build(world)
-        .with(StaticCollider)
-        .build();
+
     let floor = build_shape
         .clone()
         .with_translation(-V::one_hot(1) * 1.0)
         .with_rotation(-1, 1, PI / 2.)
-        .with_color(BLUE);
-    floor
-        .clone()
-        .with_translation(-V::one_hot(0) * 2.0)
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    floor
-        .clone()
-        .with_translation(-V::one_hot(0) * 2.0 - V::one_hot(-1) * 2.0)
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    floor
-        .clone()
-        .with_translation(V::one_hot(1) * (2.0 * sin) - V::one_hot(-1) * (2.0 + 2.0 * cos))
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    floor
-        .clone()
-        .with_translation(V::one_hot(1) * (2.0 * sin) - V::one_hot(-1) * (4.0 + 2.0 * cos))
-        .with_color(MAGENTA)
-        .build(world)
-        .with(StaticCollider)
-        .build();
-    floor.build(world).with(StaticCollider).build();
-    build_shape
-        .clone()
-        .with_translation(V::one_hot(1) * 1.0)
-        .with_rotation(-1, 1, -PI / 2.)
-        .with_color(YELLOW)
-        .build(world)
-        .with(StaticCollider)
-        .build();
+        .with_color(BLUE)
+        .with_collider(Some(StaticCollider));
+
+    let walls = [
+        build_wall(
+            V::one_hot(-1) * (-1.0 - cos) + V::one_hot(1) * (sin - 1.0),
+            (-1, 1, PI / 2.0 - theta),
+            RED,
+        ),
+        build_wall(V::one_hot(-1) * 1.0, (0, -1, PI), GREEN),
+        build_wall(V::one_hot(0) * 1.0, (0, -1, PI / 2.), ORANGE),
+        build_wall(-V::one_hot(0) * 1.01, (0, -1, 3.0 * PI / 2.), CYAN),
+    ];
+    let floors = [
+        floor.clone().with_translation(-V::one_hot(0) * 2.0),
+        floor
+            .clone()
+            .with_translation(-V::one_hot(0) * 2.0 - V::one_hot(-1) * 2.0),
+        floor
+            .clone()
+            .with_translation(V::one_hot(1) * (2.0 * sin) - V::one_hot(-1) * (2.0 + 2.0 * cos)),
+        floor
+            .clone()
+            .with_translation(V::one_hot(1) * (2.0 * sin) - V::one_hot(-1) * (4.0 + 2.0 * cos))
+            .with_color(MAGENTA),
+        floor,
+        build_shape
+            .clone()
+            .with_translation(V::one_hot(1) * 1.0)
+            .with_rotation(-1, 1, -PI / 2.)
+            .with_color(YELLOW),
+    ];
+
+    walls
+        .into_iter()
+        .map(|f| f(build_shape.clone()))
+        .chain(floors)
 }
 pub fn build_test_level<V>(world: &mut World, ref_shapes: &mut RefShapes<V>)
 where
@@ -127,5 +110,7 @@ where
                     frame_vertis,
                 }),
             });
-    build_test_walls(&build_shape, world);
+    build_test_walls(&build_shape).into_iter().for_each(|b| {
+        b.build(world).build();
+    });
 }
